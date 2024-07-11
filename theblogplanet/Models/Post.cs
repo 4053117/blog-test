08879cff6d9030d960dc4b355ef2a0d69be1ebdd
4053117 <175152532+4053117@users.noreply.github.com>
2024-07-12 01:49:43 +0200

    V0.5
    Most functionality done.
    Some QoL issues + remaining features (Auth, Swagger, Admin)

---
+++ theblogplanet/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace theblogplanet.Models
+{
+    public class Post
+    {
+
+        public int PostId { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "The title must be 2-60 characters."), ]
+        public string PostTitle { get; set; }
+
+        [Required]
+        [StringLength(1500, MinimumLength = 20, ErrorMessage = "Posts must be 20-1500 characters.")]
+        public string PostContent { get; set; }
+
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public Post()
+        {
+            
+        }
+    }
+}
